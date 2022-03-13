e637359048d5525eff9d231c58726fa4ecd34b13
Posashkov AS <alexpos@list.ru>
2022-03-13 22:22:17 +0500

    Step 8. Game brain-progression

---
+++ src/Games/Progression.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Brain\Games\Progression;
+
+use function Brain\Games\Engine\greetings;
+use function Brain\Games\Engine\goodbye;
+use function Brain\Games\Engine\askQuestions;
+
+function startGame()
+{
+    $name = greetings('What number is missing in the progression?');
+
+    $questionsAndAnswersArray = generateQuestionsArray();
+
+    $hasErrors = askQuestions($questionsAndAnswersArray);
+
+    goodbye($hasErrors, $name);
+}
+
+function generateQuestionsArray($countNumbers = 3)
+{
+    $returnArray = [];
+
+    for ($i = 0; $i < $countNumbers; $i++) {
+        $progressionStep = rand(1, 10);
+        $numbersArray = range(1, $progressionStep * 10, $progressionStep);
+        $hiddenNumberIndex = rand(1, 10);
+        $hiddenNumber = $numbersArray[$hiddenNumberIndex];
+        $numbersArray[$hiddenNumberIndex] = '..';
+
+        $returnArray[] = [implode(' ', $numbersArray), $hiddenNumber];
+    }
+
+    return $returnArray;
+}
